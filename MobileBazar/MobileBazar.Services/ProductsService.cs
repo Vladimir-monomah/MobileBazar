2d363de2cf9aaa21200b1767fc2561b3ec908107
Владимир Быков <v.bykov@bars.group.ost>
2021-05-07 23:24:01 +0300

    Создана таблица товаров, осуществлен поиск

---
+++ MobileBazar/MobileBazar.Services/ProductsService.cs
@@ -0,0 +1,58 @@
+﻿using MobileBazar.Database;
+using MobileBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MobileBazar.Services
+{
+    public class ProductsService
+    {
+        public Product GetProduct(int ID)
+        {
+            using (var context = new MBContext())
+            {
+                return context.Products.Find(ID);
+            }
+        }
+
+        public List<Product> GetProducts()
+        {
+            using (var context = new MBContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+
+        public void SaveProduct(Product product)
+        {
+            using(var context=new MBContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new MBContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+
+        public void DeleteProduct(int ID)
+        {
+            using (var context = new MBContext())
+            {
+                var product = context.Products.Find(ID);
+
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ MobileBazar/MobileBazar.Web/Controllers/ProductController.cs
@@ -0,0 +1,47 @@
+﻿using MobileBazar.Entities;
+using MobileBazar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MobileBazar.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productsService = new ProductsService();
+
+        // GET: Product
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        public ActionResult ProductTable(string search)
+        {
+            var products = this.productsService.GetProducts();
+
+            if (string.IsNullOrEmpty(search) == false)
+            {
+                products = products.Where(
+                    p => p.Name != null && p.Name.ToLower().Contains(search.ToLower())).ToList();
+            }
+
+            return this.PartialView(products);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return this.PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            this.productsService.SaveProduct(product);
+            return this.RedirectToAction("ProductTable");
+        }
+    }
+}
